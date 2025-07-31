bdf9bddb64fefda0c08f9fe9d2ce81e49b3246e0
syg0629 <syg0629@hanmail.net>
2025-08-01 00:14:19 +0900

    feat: 로그인 및 로그아웃 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,87 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import gachachaLogo from "../assets/gachachaLogo.jpeg";
 import "../styles/Header.css";
+import {
+  // type User,
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { app } from "../../firebase";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const { pathname } = useLocation();
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigate("/login");
+      } else if (user && pathname === "/login") {
+        navigate("/");
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [pathname]);
+
+  const onClickAuth = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        setUserData(result.user);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const onClickLogout = () => {
+    signOut(auth)
+      .then(() => {
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
   return (
     <header className="header">
       <div className="header-content">
-        <div className="logo">
+        <Link to={"/"} className="logo">
           <img src={gachachaLogo} />
           가차차
-        </div>
+        </Link>
         <div className="nav-buttons">
-          <Link className="btn primary" to={`/login`}>
-            로그인
-          </Link>
+          {pathname === "/login" ? (
+            <div className="btn primary" onClick={onClickAuth}>
+              로그인
+            </div>
+          ) : (
+            <div className="sign-out">
+              {userData?.photoURL && (
+                <div className="user-actions">
+                  <Link className="btn" to={`/write`}>
+                    글 쓰기
+                  </Link>
+                  <img
+                    className="user-img"
+                    src={userData.photoURL}
+                    alt="user photo"
+                  />
+                </div>
+              )}
+              <div className="dropdown">
+                <span onClick={onClickLogout}>로그아웃</span>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </header>
