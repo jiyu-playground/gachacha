5929906f51dd2edeb1bc86bdaa94a53ca8fde974
syg0629 <syg0629@hanmail.net>
2025-08-01 18:08:52 +0900

    feat: Firebase 연동 및 사용자 인증 추가

--- src/components/Write.tsx
+++ src/components/Write.tsx
@@ -1,4 +1,76 @@
+import { useNavigate } from "react-router-dom";
+import "../styles/Write.css";
+import { useEffect, useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useAuth } from "../hooks/useAuth";
+
+type newPost = {
+  userId: string;
+  userName: string;
+  spot: string;
+  text: string;
+  image: File | null;
+  date: string;
+};
+
 const Write = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      alert("글쓰기는 로그인이 필요합니다.");
+      return navigate("/");
+    }
+  }, [user, navigate]);
+
+  const [newPost, setNewPost] = useState<newPost>({
+    userId: user?.uid || "",
+    userName: user?.displayName,
+    spot: "",
+    text: "",
+    image: null,
+    date: new Date().toLocaleDateString(),
+  });
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!user) {
+      alert("글쓰기는 로그인이 필요합니다.");
+      return;
+    }
+
+    if (!newPost.spot.trim() || !newPost.text.trim()) {
+      alert("가챠 위치와 내용을 입력해주세요.");
+      return;
+    }
+
+    try {
+      await addDoc(collection(db, "posts"), {
+        userId: user.uid,
+        userName: user.displayName,
+        text: newPost.text,
+        spot: newPost.spot,
+        createdAt: new Date(),
+      });
+
+      alert("게시물이 등록되었습니다.");
+      navigate("/list");
+    } catch (err) {
+      console.error(err);
+      alert("게시물 등록을 실패하였습니다.");
+    }
+  };
+
+  const onChangeWrite = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setNewPost((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
     <div>
       <div className="post-form">
@@ -10,6 +82,8 @@ const Write = () => {
               type="text"
               className="form-control"
               placeholder="예: 강남역 가챠샵"
+              onChange={onChangeWrite}
+              name="spot"
             />
           </div>
           <div className="form-group">
@@ -17,15 +91,29 @@ const Write = () => {
             <textarea
               className="form-control"
               placeholder="어떤 가챠를 득템했나요?"
+              name="text"
+              onChange={onChangeWrite}
             ></textarea>
           </div>
           <div className="form-group">
             <label className="form-label">사진 업로드</label>
-            <input type="file" className="form-control" accept="image/*" />
+            <input
+              type="file"
+              className="form-control"
+              accept="image/*"
+              name="image"
+              onChange={onChangeWrite}
+            />
           </div>
-          <button type="submit" className="btn primary">
-            게시하기
-          </button>
+          <div className="flex-div">
+            <button
+              type="submit"
+              className="btn primary center"
+              onClick={onSubmit}
+            >
+              게시하기
+            </button>
+          </div>
         </form>
       </div>
     </div>
